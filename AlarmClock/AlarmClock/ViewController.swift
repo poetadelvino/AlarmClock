dd02e3ab4e28d48da1900af5adeddd50989e34d2
poetadelvino <almanegraecm@gmail.com>
2015-06-24 20:50:31 -0700

    midway

--- AlarmClock/AlarmClock/ViewController.swift
+++ AlarmClock/AlarmClock/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 // this is vincent's comment
 
@@ -18,35 +19,100 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // global variables:
+    
+    var hourInt: Int = 0
+    var minInt: Int = 0
+    var secInt: Int = 0
+    
+    var timeToWakeUp = NSDateComponents()
+    
+    // sound properties:
+    
+    var audioPlayer: AVAudioPlayer?
     
     // text buttons:
     
+    // wake up time:
+    
+    
     @IBOutlet weak var wakeUpTime: UITextField!
+    
     
     @IBOutlet weak var SnoozeTime: UITextField!
     
     @IBOutlet weak var Difficulty: UITextField!
     
     // music buttons:
+   
+    //    low E-note (green, upper left, an octave lower than blue);
+    
+    
+    @IBAction func lowE(sender: UIButton) {
+        
+        // here's a code from stack overflow on getting a coin sound:
+        // Grab the path, make sure to add it to your project!
+        var pianoSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("CasioPiano", ofType: "wav")!)
+        
+        // for testing..
+        var error: NSError?
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: pianoSound, error: &error)
+        //print the result of the file retrieval
+        println(error)
+        audioPlayer?.prepareToPlay()
+        
+        audioPlayer?.play()
+    }
     
 //    E-note (blue, lower right);
+    
 //    C♯-note (yellow, lower left);
+    
 //    A-note (red, upper right).
-//    E-note (green, upper left, an octave lower than blue);
-    
-    
+
+    //av player
     
     
     
     // snooze/off buttons:
     
 
+
     
+
     
+    func readTextTimeAndStoreAsInts() {
+ 
+        var wakeUpTimeString: String = wakeUpTime.text
+        
+        println(" here's the wakeup time: \(wakeUpTimeString)")
+        
+        func textToInts (textTime: String) {
+        
+            //var fullName = "First Last"
+            var textTimeArray = split(textTime) {$0 == ":"}
+            var hourText: String = textTimeArray[0]
+            var minText: String? = textTimeArray.count > 1 ? textTimeArray[1] : nil
+            
+            hourInt = hourText.toInt()!
+            minInt = minText!.toInt()!
+            
+            println("\(hourInt) : \(minInt)")
+            
+        }  // end textToInts
+
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        readTextTimeAndStoreAsInts()
+        //now store wakeup time as a time
+        timeToWakeUp.hour = hourInt
+        timeToWakeUp.minute = minInt
+        
     }
 
     override func didReceiveMemoryWarning() {
